4d6252489813a557fc2f8c828497529aaee9d539
unknown <shaheelabbasi456@gmail.com>
2024-10-11 12:13:19 +0500

    added docker compsoe file

--- Db/connect.js
+++ Db/connect.js
@@ -1,10 +1,12 @@
 // import mongoose from "mongoose";
 const mongoose=require("mongoose")
-
+console.log("mongo url is ",process.env.MONGO_URL)
 const connectDb=async()=>{
 
     try {
-       const connectionInstance=await mongoose.connect("mongodb://localhost:27017/AuthService")
+       const connectionInstance=await mongoose.connect(process.env.MONGO_URL, {
+        useUnifiedTopology: true,
+      })
         console.log("successfully connected to the database")
     } catch (error) {
         console.log("Error connecting to the database",error)
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const app = require('./app.js');
 const { connectDb } = require('./Db/connect.js');
-
+// console.log("mongourl is ",process.env.MONGO_URL)
 connectDb()
   .then(() => {
     app.listen(5000, () =>
-      console.log(`Auth Server is running on the port 9000`)
+      console.log(`Auth Server is running on the port 5000`)
     );
   })
   .catch(err => console.log(err));
